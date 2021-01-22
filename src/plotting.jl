3252ce2982060aba299cdd5c4a2981f8adc5da4d
Joel Dahne <joel@dahne.eu>
2021-01-22 23:29:00 +0100

    Update plotting

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,45 +1,64 @@
 """
     mesh_triangle_midpoints(N::Integer = 36)
 
-Return the midpoints of the triangles in the mesh.
+Return the midpoints of the triangles in the mesh. This is NOT
+rigorous.
+
 """
 function mesh_triangle_midpoints(N::Integer = 36)
     num_triangles = N^2
 
-    xs = zeros(num_triangles)
-    ys = zeros(num_triangles)
+    xs = zeros(6num_triangles)
+    ys = zeros(6num_triangles)
 
     # Height and side length of each triangle
-    height = sqrt(3)/2N
-    side = 1/N
+    height = sqrt(3) / 2N
+    side = 1 / N
+
+    # Compute the x and y coordinates of the mesh-triangles in the
+    # first big triangle.
 
     # x and y increment along each row
-    x_inc = cospi(2//3)*side/2
-    y_inc = sinpi(2//3)*side/2
+    x_inc = cospi(2 // 3) * side / 2
+    y_inc = sinpi(2 // 3) * side / 2
 
-    for row in 1:N
+    for row = 1:N
         row_start = (row - 1)^2
 
         # First part of the triangles
 
         # x and y coordinates for the first triangle in the row
-        x₀ = side*(row - 1) + side/2
-        y₀ = side*tan(π/6)/2
+        x₀ = side * (row - 1) + side / 2
+        y₀ = side * tan(π / 6) / 2
 
-        for i in 1:2:(2row - 1)
-            xs[row_start + i] = x₀ + (i - 1)*x_inc
-            ys[row_start + i] = y₀ + (i - 1)*y_inc
+        for i = 1:2:(2row-1)
+            xs[row_start+i] = x₀ + (i - 1) * x_inc
+            ys[row_start+i] = y₀ + (i - 1) * y_inc
         end
 
         # Second part of the triangles
 
         # x and y coordinates for the second triangle in the row
-        x₀ = side*(row - 1)
-        y₀ = height - side*tan(π/6)/2
+        x₀ = side * (row - 1)
+        y₀ = height - side * tan(π / 6) / 2
 
-        for i in 2:2:(2row - 1)
-            xs[row_start + i] = x₀ + (i - 2)*x_inc
-            ys[row_start + i] = y₀ + (i - 2)*y_inc
+        for i = 2:2:(2row-1)
+            xs[row_start+i] = x₀ + (i - 2) * x_inc
+            ys[row_start+i] = y₀ + (i - 2) * y_inc
+        end
+    end
+
+    # Compute the x and y coordinates of the mesh-triangles in the
+    # other triangles by rotation
+    for triangle = 2:6
+        θ = (triangle - 1) * π / 3
+        s, c = sincos(θ)
+
+        for i = 1:num_triangles
+            x = xs[i]
+            y = ys[i]
+            xs[num_triangles*(triangle-1)+i] = x * c - y * s
+            ys[num_triangles*(triangle-1)+i] = x * s + y * c
         end
     end
 
@@ -55,40 +74,62 @@ triangles centered at these points.
 function triangle_edge(c1, c2)
     v = c2 - c1
 
-    M = [cospi(1//3) -sinpi(1//3); sinpi(1//3) cospi(1//3)]
+    M = [cospi(1 // 3) -sinpi(1 // 3); sinpi(1 // 3) cospi(1 // 3)]
 
     # c1 + (v rotated π/3 positive direction)
-    e1 = c1 + M*v
+    e1 = c1 + M * v
     # c1 + (v rotated -π/3 positive direction)
-    e2 = c1 + inv(M)*v
+    e2 = c1 + inv(M) * v
 
     return e1, e2
 end
 
-function plot_mesh(N::Integer)
+function plot_mesh(N::Integer; excluded = BitSet([7]), marked = BitSet([8]))
     xs, ys = mesh_triangle_midpoints(N)
     edge_to_triangle1, edge_to_triangle2 = edges_to_triangles(N)
 
-    pl = plot([0, 1, 0.5, 0], [0, 0, sqrt(3)/2, 0], aspect_ratio = true, legend = :none)
-    scatter!(pl, xs, ys)
+    num_triangles = N^2
+    edges = collect(zip(edge_to_triangle1, edge_to_triangle2))
+    indices = findall(
+        ((t1, t2),) ->
+            !((t1 % num_triangles ∈ excluded) || (t2 % num_triangles ∈ excluded)),
+        edges,
+    )
+    edge_to_triangle1 = edge_to_triangle1[indices]
+    edge_to_triangle2 = edge_to_triangle2[indices]
 
-    num_edges = div(3N^2 - 3N, 2)
-    xs_edges = zeros(2, num_edges)
-    ys_edges = zeros(2, num_edges)
-    i = 1
+    xs_edges = zeros(2, length(edge_to_triangle1))
+    ys_edges = zeros(2, length(edge_to_triangle1))
+    colors = permutedims([
+        ifelse(
+            edge_to_triangle1[i] % num_triangles ∈ marked ||
+            edge_to_triangle2[i] % num_triangles ∈ marked,
+            :red,
+            :blue,
+        ) for i in eachindex(edge_to_triangle1)
+    ])
 
-    for (t1, t2) in zip(edge_to_triangle1, edge_to_triangle2)
-        if t1 <= N^2 && t2 <= N^2
-            e1, e2 = PaynePolygon.triangle_edge([xs[t1], ys[t1]], [xs[t2], ys[t2]]);
-            xs_edges[1, i] = e1[1]
-            xs_edges[2, i] = e2[1]
-            ys_edges[1, i] = e1[2]
-            ys_edges[2, i] = e2[2]
-            i += 1
-        end
+    for (i, (t1, t2)) in enumerate(zip(edge_to_triangle1, edge_to_triangle2))
+        e1, e2 = PaynePolygon.triangle_edge([xs[t1], ys[t1]], [xs[t2], ys[t2]])
+        xs_edges[1, i] = e1[1]
+        xs_edges[2, i] = e2[1]
+        ys_edges[1, i] = e1[2]
+        ys_edges[2, i] = e2[2]
     end
 
-    plot!(pl, xs_edges, ys_edges, color = :red)
+    h = sqrt(3) / 2
+    pl = plot(
+        [1, 0.5, -0.5, -1, -0.5, 0.5, 1],
+        [0, h, h, 0, -h, -h, 0],
+        aspect_ratio = true,
+        legend = :none,
+    )
+
+    scatter!(pl, xs, ys, ms = 1, aspect_ratio = true, legend = :none)
+
+    plot!(pl, xs_edges, ys_edges, color = colors)
+
+    plot!(pl, [1, 0.5, 0, 1], [0, h, 0, 0])
 
     return pl
 end
