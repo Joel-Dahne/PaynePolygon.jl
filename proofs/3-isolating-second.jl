d1762d00c054f7bf34449bcb7343e1b2d7faf4df
Joel Dahne <joel@dahne.eu>
2021-02-11 15:38:25 +0100

    Add empty proofs/isolating-second.jl

---
+++ proofs/3-isolating-second.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 23d68114-6c76-11eb-373e-7b8358f16372
+md"# Isolating the second eigenvalue"
+
+# ╔═╡ Cell order:
+# ╠═23d68114-6c76-11eb-373e-7b8358f16372
