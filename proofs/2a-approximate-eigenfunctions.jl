a3016c0d33b543d3b246da1616a95da047e79902
Joel Dahne <joel@dahne.eu>
2021-02-22 10:44:09 +0100

    Slightly update 2-approximate-eigenfunctions.jl and rename it

--- proofs/2a-approximate-eigenfunctions.jl
+++ proofs/2a-approximate-eigenfunctions.jl
@@ -18,20 +18,14 @@ We compute four eigenfunction `u₁`, `u₂`, `u₃` and `u₄`, which require d
 # ╔═╡ a9c7d5e2-6b7d-11eb-1f70-996bf19341c9
 import MethodOfParticularSolutions: example_domain_goal_v1
 
-# ╔═╡ 395623fc-6baf-11eb-29d5-979c107cc16d
-md"The domain is parametrised by three values `N`, `d` and `h`. The version in the article uses"
-
-# ╔═╡ 591d9870-6baf-11eb-237e-a104e89dc7f6
-N, d, h = 27, 11, 6
-
 # ╔═╡ c2c6e4d2-6a26-11eb-2b0c-31de8f876374
-md"We start by defining the eigenfunctions four eigenfunctions we will use. Both $u_1$ and $u_2$ have a six-fold even symmetry, which we get by giving `symmetry_class = 1`. $u_3$ and $u_4$ have similar but slighly different symmetries, which we get with `symmetry_class = 2` and `symmetry_class = 3` respectively."
+md"We start by defining the four eigenfunctions we will use. Both $u_1$ and $u_2$ have a six-fold even symmetry, which we get by giving `symmetry_class = 1`. $u_3$ and $u_4$ have similar but slighly different symmetries, which we get with `symmetry_class = 2` and `symmetry_class = 3` respectively."
 
 # ╔═╡ 345db9e6-6bac-11eb-1bad-7dd0c26b0449
 symmetry_classes = [1, 1, 2, 3]
 
 # ╔═╡ 659d4aa0-6b7d-11eb-12b5-f9adc7ba65cb
-domains, us = begin
+domains, us = let N = 27, d = 11, h = 6
     domains_us = [
         example_domain_goal_v1(N, d, h, T = Float64; symmetry_class) for
         symmetry_class in symmetry_classes
@@ -44,7 +38,7 @@ end
 md"We have the following approximations for the eigenvalues"
 
 # ╔═╡ 0a9efd78-6b7e-11eb-1775-0db146884b8d
-λs = [31.0432, 63.2104, 63.7259, 63.7259]
+λs = [31.0432, 63.20833838, 63.7259, 63.7259]
 
 # ╔═╡ 1701c14e-6b7f-11eb-27ef-dd57dff8f572
 md"We can plot the domain that is being used"
@@ -63,6 +57,12 @@ md"And for references we have the following values for `sum(u.orders)`"
 
 # ╔═╡ 9cdd5c42-6b81-11eb-2c36-6b248fc18a64
 [sum(u.orders) for u in us]
+
+# ╔═╡ fb3ee9ca-70f4-11eb-24ae-7fbacb3c583b
+md"So the number of free coefficients for each eigenfunction is"
+
+# ╔═╡ 0308bc62-70f5-11eb-21fe-7b14e9f01f7b
+[n * sum(u.orders) for (n, u) in zip(ns, us)]
 
 # ╔═╡ b964656e-6b81-11eb-05f2-79d153d02e39
 md"We are now ready to compute the approximations. We compute the the approximation for each eigenfunction and then an approximate enclosure for the eigenvaleu."
@@ -169,7 +169,7 @@ for i in eachindex(us)
 end
 
 # ╔═╡ e5e2103e-6b88-11eb-3aff-61834843c2ad
-md"The eigenfunctions can then be reconstructed at a later time with 
+md"The eigenfunctions can then be reconstructed at a later time with
 ```
 domain, u, λ = PaynePolygon.load_eigenfunctions(\"../data/approximate-eigenfunctions-$i.jld\")
 ```
@@ -207,8 +207,6 @@ eigenfunctionheatmap(
 # ╟─016dcf3e-685b-11eb-0b70-3f5dfe98193a
 # ╠═7ed715a0-6b7d-11eb-24c7-21588b2d267c
 # ╠═a9c7d5e2-6b7d-11eb-1f70-996bf19341c9
-# ╟─395623fc-6baf-11eb-29d5-979c107cc16d
-# ╠═591d9870-6baf-11eb-237e-a104e89dc7f6
 # ╟─c2c6e4d2-6a26-11eb-2b0c-31de8f876374
 # ╠═345db9e6-6bac-11eb-1bad-7dd0c26b0449
 # ╠═659d4aa0-6b7d-11eb-12b5-f9adc7ba65cb
@@ -220,6 +218,8 @@ eigenfunctionheatmap(
 # ╠═5aefc90e-6b80-11eb-06c1-0ff8b3b4647d
 # ╟─ac3c2e10-6b80-11eb-2763-c3b7a7e3d582
 # ╠═9cdd5c42-6b81-11eb-2c36-6b248fc18a64
+# ╠═fb3ee9ca-70f4-11eb-24ae-7fbacb3c583b
+# ╠═0308bc62-70f5-11eb-21fe-7b14e9f01f7b
 # ╟─b964656e-6b81-11eb-05f2-79d153d02e39
 # ╠═07b535a0-6b80-11eb-37d2-5f39d69572db
 # ╠═f8cdf9c6-6b9d-11eb-3330-898302a734e5
